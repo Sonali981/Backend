0d838968fbe376e774943834d11985065b1e543b
Sonali981 <73358254+Sonali981@users.noreply.github.com>
2021-08-24 01:36:03 +0530

    Update CrudRepo.java

--- Backend/src/main/java/com/backend/project/repository/CrudRepo.java
+++ Backend/src/main/java/com/backend/project/repository/CrudRepo.java
@@ -3,7 +3,7 @@ package com.backend.project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.project.model.Product;
-//repo
+
 public interface CrudRepo extends JpaRepository<Product, Integer> {
 
 }
